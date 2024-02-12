6ed3f5f2723853e7a4e29902d4cf02de5c63832f
Durvesh Patil <71640344+durveshvpatil01@users.noreply.github.com>
2024-02-12 13:39:04 +0530

    hash

---
+++ Collection/src/com/edubridge/set/HashSetDemo.java
@@ -0,0 +1,35 @@
+package com.edubridge.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		Set<String>obj=new  HashSet<>();
+		obj.add("Akshay");
+		obj.add("Abhishek");
+		obj.add("Aftab");
+		obj.add("Atul");
+		System.out.println(obj);
+		
+		Set<String>obj1=new  LinkedHashSet<>();
+		obj1.add("Akshay");
+		obj1.add("Abhishek");
+		obj1.add("Aftab");
+		obj1.add("Atul");
+		System.out.println(obj1);
+		
+		
+		Set<String>obj2=new  TreeSet<>();
+		obj2.add("Akshay");
+		obj2.add("Abhishek");
+		obj2.add("Aftab");
+		obj.add("Atul");
+		System.out.println(obj2);
+	}
+
+}
